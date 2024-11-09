63dbc6bdc946e78846b72076cfbe14c41079f733
Shaikh Fazle Kareem <165131929+Kareem1516@users.noreply.github.com>
2024-11-09 21:44:09 +0530

    Add files via upload

---
+++ backend/utils/testUsers.js
@@ -0,0 +1,13 @@
+const testUsers = [{ username: 'masteradmin', email: 'admin@admin.com', password: 'admin123' , tribe : "The_Sentinal"} ,
+{ username: 'shashank_manan123', email: 'shashankmanan@gmail.com', password: 'shashank' , tribe : "The_Peacemaker"},
+{ username : "masteradmin123" , email : "admin@admin123.com" , password : "admin123" , tribe : "The_Peacemaker"} ,
+{ username : "testuser123" , email : "shashank@admin.com" , password : "admin123" , tribe : "The_Explorer"} ,
+{ username : "jackrosie" , email : "admin12@admin123.com" , password : "admin123" , tribe : "The_Sentinal"}
+
+]
+/*
+testCases 
+2. directly update
+1. create a profile and then update it
+
+*/
